e5270e7f79e2be161be9ba629f571c93156f522d
Daniel <daniel.forde001@gmail.com>
2014-05-10 11:41:06 +0200

    Improved algorithm for Problem 5

--- src/include/ProblemSolutions/Problem005.hpp
+++ src/include/ProblemSolutions/Problem005.hpp
@@ -7,13 +7,34 @@
 #include <iostream>
 #include <cstdint>
 #include <cmath>
+#include <vector>
 #include <map>
 
 bool Problem5 ()
 {
-	uint32_t ans;
+	uint32_t ans=1;
+	std::vector<uint8_t> primes_under_twenty = {2,3,5,7,11,13,17,19};
+	std::map<uint8_t, uint8_t> factor_frequency;
+	uint8_t temp, count;
 	
-	ans = 2*2*2*2 * 3*3 * 5 * 7 * 11 * 13 * 17 * 19;
+	for (std::vector<uint8_t>::iterator n=primes_under_twenty.begin(); n!=primes_under_twenty.end(); n++)	factor_frequency[*n] = 0;
+	
+	for (uint8_t i=2; i<21; i++)
+	{
+		temp = i;
+		for (std::vector<uint8_t>::iterator n=primes_under_twenty.begin(); n!=primes_under_twenty.end(); n++)
+		{
+			count = 0;
+			while(temp%*n==0)
+			{
+				temp = temp / *n;
+				count++;
+			}
+			if (count>factor_frequency[*n]) factor_frequency[*n] = count;
+		}
+	}
+	
+	for (std::vector<uint8_t>::iterator n=primes_under_twenty.begin(); n!=primes_under_twenty.end(); n++)	ans *= pow(*n, factor_frequency[*n]);
 	
 	std::cout << "\nProblem 005: " << ans << "\n";
 	
