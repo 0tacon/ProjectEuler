a0468b52bc5c1f1db13db90369760f76a7b6dfc8
0tacon <daniel.forde001@gmail.com>
2014-08-01 21:26:02 +0200

    Completed Problem 49

--- src/include/ProblemSolutions/Problem049.h
+++ src/include/ProblemSolutions/Problem049.h
@@ -32,6 +32,7 @@ along with ProjectEuler.  If not, see <http://www.gnu.org/licenses/>.
 #include <string>
 #include <vector>
 #include <map>
+#include <utility>
 #include <algorithm>
 
 #ifndef _GETALLPRIMESBELOW_
@@ -40,61 +41,105 @@ along with ProjectEuler.  If not, see <http://www.gnu.org/licenses/>.
 
 bool Problem49()
 {
-    std::string full_str = "123456789", trunc_str;
-    std::vector<std::string> permutations, str_primes;
     std::vector<uint32_t> primes = GetAllPrimesBelow(10000);
+    std::map<std::string, bool> str_primes;
+    std::map<uint16_t, std::vector<uint32_t> > sequences;
 
-    for (std::vector<uint32_t>::iterator itr = primes.begin(); itr != primes.end(); itr++)
-        str_primes.push_back(std::to_string(*itr));
+    for (std::vector<uint32_t>::iterator prime = primes.begin(); prime != primes.end(); prime++)
+        if (*prime > 999)
+            str_primes[std::to_string(*prime)] = false;
 
-    trunc_str = full_str;
-    trunc_str.erase(std::prev(trunc_str.end(), 5), trunc_str.end());
-
-    std::next_permutation(full_str.begin(), full_str.end());
-
-    while(full_str != "123456789")
+    uint16_t sequences_counter = 0;
+    for (std::map<std::string, bool>::iterator prime = str_primes.begin(); prime != str_primes.end(); prime++)
     {
-        trunc_str = full_str;
-        trunc_str.erase(std::prev(trunc_str.end(), 5), trunc_str.end());
-
-        if (std::count(permutations.begin(), permutations.end(), trunc_str) == 0 && std::count(str_primes.begin(), str_primes.end(), trunc_str) == 1)
-            permutations.push_back(trunc_str);
-
-        std::next_permutation(full_str.begin(), full_str.end());
-    }
-
-    std::map<std::string, bool > map_perms;
-    for (std::vector<std::string>::iterator a = permutations.begin(); a != permutations.end(); a++)
-        map_perms[*a] = false;
-
-    std::map<uint16_t, std::vector<uint16_t> > perm_sequences;
-
-    uint16_t counter = 0;
-    for (std::vector<std::string>::iterator a = permutations.begin(); a != permutations.end(); a++)
-    {
-        if (map_perms[*a] != true)
+        if (str_primes[prime->first] == false)
         {
-            map_perms[*a] = true;
-            perm_sequences[counter].push_back(std::stoi(*a));
+            sequences[sequences_counter].push_back(std::stoi(prime->first));
+            str_primes[prime->first] = true;
 
-            for (std::vector<std::string>::iterator b = std::next(a, 1); b != permutations.end(); b++)
-                if (map_perms[*b] != true && *a != *b && std::is_permutation(a->begin(), a->end(), b->begin()))
+            for (std::map<std::string, bool>::iterator other_prime = std::next(prime, 1); other_prime != str_primes.end(); other_prime++)
+            {
+                if (std::is_permutation(prime->first.begin(), prime->first.end(), other_prime->first.begin()))
                 {
-                    map_perms[*b] = true;
-                    perm_sequences[counter].push_back(std::stoi(*b));
+                    sequences[sequences_counter].push_back(std::stoi(other_prime->first));
+                    str_primes[other_prime->first] = true;
                 }
+            }
+
+            sequences_counter++;
         }
-
-        counter++;
     }
 
-    for (std::map<uint16_t, std::vector<uint16_t> >::iterator itr = perm_sequences.begin(); itr != perm_sequences.end(); itr++)
+    uint16_t filtered_sequences_counter = 0;
+    std::map<uint16_t, std::vector<uint32_t> > filtered_sequences;
+    for (std::map<uint16_t, std::vector<uint32_t> >::iterator seq = sequences.begin(); seq != sequences.end(); seq++)
     {
-        printf("\n");
-        for (std::vector<uint16_t>::iterator num = itr->second.begin(); num != itr->second.end(); num++)
-            printf("%d, ", *num);
+        if (seq->second.size() > 2)
+        {
+            std::vector<uint32_t> seq_differences;
+            std::vector<std::pair<uint32_t, uint32_t> > filtered_sequence;
+            uint16_t filtered_sequence_counter;
+
+            uint8_t forward_shift = 0;
+            for (; forward_shift < seq->second.size() - 2; forward_shift++)
+            {
+                filtered_sequence_counter = 1 + forward_shift;
+
+                std::vector<uint32_t>::iterator other_num = std::next(seq->second.begin(), forward_shift);
+
+                for (std::vector<uint32_t>::iterator num = std::next(other_num, 1); num != seq->second.end(); num++)
+                    seq_differences.push_back(*num - *other_num);
+
+                for (std::vector<uint32_t>::iterator diff = seq_differences.begin(); diff != seq_differences.end(); diff++)
+                {
+                    uint8_t number_multiples = 1;
+                    filtered_sequence.push_back(std::make_pair(filtered_sequence_counter, *diff));
+                    filtered_sequence_counter++;
+
+                    for (std::vector<uint32_t>::iterator other_diff = std::next(diff, 1); other_diff != seq_differences.end(); other_diff++)
+                    {
+                        if (*other_diff%*diff == 0)
+                        {
+                            number_multiples++;
+
+                            if (*other_diff == *diff * number_multiples)
+                            {
+                                filtered_sequence.push_back(std::make_pair(filtered_sequence_counter, *diff));
+                                filtered_sequence_counter++;
+                            }
+                        }
+                    }
+
+                    if (number_multiples > 1 && filtered_sequence.size() == number_multiples)
+                        break;
+
+                    filtered_sequence.clear();
+                }
+
+                if (filtered_sequence.size() == 0)
+                    seq_differences.clear();
+                else
+                    break;
+            }
+
+            if (filtered_sequence.size() > 0)
+            {
+                filtered_sequences[filtered_sequences_counter].push_back(seq->second[forward_shift]);
+
+                for (std::vector<std::pair<uint32_t, uint32_t> >::iterator filtered_seq_num = filtered_sequence.begin(); filtered_seq_num != filtered_sequence.end(); filtered_seq_num++)
+                    filtered_sequences[filtered_sequences_counter].push_back(seq->second[filtered_seq_num->first]);
+
+                filtered_sequences_counter++;
+            }
+        }
     }
 
-    std::printf("\nProblem 049: \n");
+    std::string final_answer = "";
+    for (std::map<uint16_t, std::vector<uint32_t> >::iterator itr = filtered_sequences.begin(); itr != filtered_sequences.end(); itr++)
+        if (itr->second[0] != 1487)
+            for (std::vector<uint32_t>::iterator num = itr->second.begin(); num != itr->second.end(); num++)
+                final_answer += std::to_string(*num);
+
+    std::printf("\nProblem 049: %s\n", final_answer.c_str());
     return true;
 }
